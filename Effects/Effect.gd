516a5b6dd81daf84c95b3ef75aad7c4315c65eec
sigped12 <112166811+sigped12@users.noreply.github.com>
2022-12-09 13:41:40 +0100

    Update

--- Effects/Effect.gd
+++ Effects/Effect.gd
@@ -1,11 +1,10 @@
-extends Node2D
-
-onready var animatedSprite = $AnimatedSprite
+extends AnimatedSprite
 
 func _ready():
-	animatedSprite.frame = 0
-	animatedSprite.play("Animate")
+	connect("animation_finished", self, "_on_animation_finished")
+	frame = 0
+	play("Animate")
 
 
-func _on_AnimatedSprite_animation_finished():
+func _on_animation_finished():
 	queue_free()
